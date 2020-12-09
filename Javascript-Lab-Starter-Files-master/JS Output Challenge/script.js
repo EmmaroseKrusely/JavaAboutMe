2f638d8066750da43736fe155a784bb7211656dc
EmmaroseKrusely <70153909+EmmaroseKrusely@users.noreply.github.com>
2020-12-09 13:06:23 -0500

    Add files via upload

---
+++ Javascript-Lab-Starter-Files-master/JS Output Challenge/script.js
@@ -0,0 +1,49 @@
+// Enter your code below to edit index.html
+
+yourName = "Emma"
+document.getElementById("yourName").innerHTML = yourName;
+
+bio = "I like music and science :)"
+document.getElementById("myBio").innerHTML = bio;
+
+college = "University of Michigan"
+document.getElementById("collegeChoice").innerHTML = college;
+
+program = "Chemistry"
+document.getElementById("programChoice").innerHTML = program;
+
+why = "I chose chemistry because I would like to go into forensics, and I chose the Universtiy of Michigan because my grandma hates Michigan and I think it would be funny."
+document.getElementById("collegeReason").innerHTML = why;
+
+title1 = "Forensic Scientist"
+document.getElementById("careerChoice").innerHTML = title1;
+
+company = "The Cleveland Police Department"
+document.getElementById("companyChoice").innerHTML = company;
+
+reason = "I love science and chemistry, so being able to work in a science field would be great. I also watch a lot of Investigation Discovery, and instead of it influencing me to be a serial killer, it made me want to work for the crime divison."
+
+document.getElementById("careerReason").innerHTML = reason;
+
+question1 = "The Document Object Model is a programming interface for HTML. It represents the page so a program can change things inside the document such as the structure, style, and content."
+
+document.getElementById("answerOne").innerHTML = question1;
+
+question2 = "Writing into an HTML element, using innerHTML, writing into the HTML output using document.write(), writing into an alert box, using window.alert(),writing into the browser console, using console.log"
+
+document.getElementById("answerTwo").innerHTML = question2;
+
+question3 ="Javascript can change all the HTML elements."
+
+document.getElementById("answerThree").innerHTML = question3;
+
+
+
+
+
+
+
+
+
+
+
